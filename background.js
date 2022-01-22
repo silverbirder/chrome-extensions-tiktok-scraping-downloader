c2ab71a889eefe376ba224cee74aadc4cb23c156
shibamoto <silverbirder@gmail.com>
2022-01-22 21:43:42 +0900

    Delete popup

--- background.js
+++ background.js
@@ -16,14 +16,32 @@ const initStorage = async () => {
     await chrome.storage.sync.set({ ts: value });
 };
 
-chrome.runtime.onInstalled.addListener(async () => {
-    chrome.webRequest.onCompleted.addListener(handleWebRequest, { urls: ["https://www.tiktok.com/*", "https://t.tiktok.com/*"] })
-    await initStorage();
-});
-
 const getCurrentTab = async () => {
     const queryOptions = { url: ["https://www.tiktok.com/*"] };
     const tabs = await chrome.tabs.query(queryOptions);
     const activeTabs = tabs.filter((t) => t.active === true)
     return activeTabs.length > 0 ? activeTabs[0] : tabs[0];
 }
+
+chrome.runtime.onInstalled.addListener(async () => {
+    chrome.webRequest.onCompleted.addListener(handleWebRequest, { urls: ["https://www.tiktok.com/*", "https://t.tiktok.com/*"] })
+    await initStorage();
+});
+
+chrome.action.onClicked.addListener(
+    async () => {
+        const storage = await chrome.storage.sync.get(['ts']);
+        const ts = storage.ts;
+        const tab = await getCurrentTab();
+        if (ts.started) {
+            ts.started = false;
+            chrome.action.setBadgeText({ text: '' });
+            chrome.tabs.sendMessage(tab.id, { "message": "end" });
+        } else {
+            ts.started = true;
+            chrome.action.setBadgeText({ text: 'ON' });
+            chrome.tabs.sendMessage(tab.id, { "message": "start" });
+        }
+        await chrome.storage.sync.set({ ts: ts });
+    }
+)
